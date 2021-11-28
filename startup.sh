2d79471eabf2cf4c9d40c73ea96757a0867e1b8b
Dylanve151 <50217470+Dylanve151@users.noreply.github.com>
2021-11-29 00:36:10 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #startup script
 
-
-
-tail -fn0 log.log
+websockify -D --web=/usr/share/novnc/ --cert=/home/debian/novnc.pem 6080 localhost:5900
+x11vnc -forever -shared -usepw -create
