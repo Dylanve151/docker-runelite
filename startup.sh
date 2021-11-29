c598f0b5b9e15fdb8216f155c5032fee2e847af4
Dylanve151 <50217470+Dylanve151@users.noreply.github.com>
2021-11-29 10:42:29 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #startup script
 
-websockify -D --web=/usr/share/novnc/ --cert=/home/root/novnc.pem 6080 localhost:5900
 Xvfb :20 -screen 0 1048x504x24
 export DISPLAY=:20
 java -jar /root/RuneLite.jar
 unset DISPLAY
+websockify -D --web=/usr/share/novnc/ --cert=/home/root/novnc.pem 6080 localhost:5900
 x11vnc -forever -shared -usepw -display :20
