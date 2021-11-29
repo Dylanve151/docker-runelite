5e6708d5e3ed5c12eb4c10ae958a9156c80223d8
Dylanve151 <50217470+Dylanve151@users.noreply.github.com>
2021-11-29 15:30:26 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #startup script
 
-if [ ! -f "~/.vnc/passwd" ]; then
-    x11vnc -storepasswd $PASSWORD ~/.vnc/passwd
+if [ ! -f "/root/.vnc/passwd" ]; then
+    x11vnc -storepasswd $PASSWORD /root/.vnc/passwd
 fi
-websockify -D --web=/usr/share/novnc/ --cert=/home/root/novnc.pem 6080 localhost:5900
+websockify -D --web=/usr/share/novnc/ --cert=/root/novnc.pem 6080 localhost:5900
 Xvfb :20 -screen 0 1048x504x24 &
 export DISPLAY=:20
 java -jar /root/RuneLite.jar
