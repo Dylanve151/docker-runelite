0278e22016fb7ca510f708be0221943e045b1dff
Dylanve151 <50217470+Dylanve151@users.noreply.github.com>
2021-11-29 14:04:13 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #startup script
 
+x11vnc -storepasswd $PASSWORD ~/.vnc/passwd
 websockify -D --web=/usr/share/novnc/ --cert=/home/root/novnc.pem 6080 localhost:5900
 Xvfb :20 -screen 0 1048x504x24 &
 export DISPLAY=:20
