19755030ef006e69925950801b6651da026cbbc8
Dylanve151 <50217470+Dylanve151@users.noreply.github.com>
2021-12-09 13:49:36 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,6 +4,11 @@
 if [ ! -f "/root/.vnc/passwd" ]; then
     x11vnc -storepasswd $PASSWORD /root/.vnc/passwd
 fi
+
+if [ -f "/tmp/.X20-lock" ]; then 
+   rm /tmp/.X20-lock -f
+fi
+
 websockify -D --web=/usr/share/novnc/ --cert=/root/novnc.pem 6080 localhost:5900
 Xvfb :20 -screen 0 $SCREENSIZE &
 export DISPLAY=:20
