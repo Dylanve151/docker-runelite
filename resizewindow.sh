b6bd5de6045428cf6a38b867ade3b3b10f9f4c5f
Dylanve151 <50217470+Dylanve151@users.noreply.github.com>
2021-12-30 11:37:42 +0100

    Create resizewindow.sh

---
+++ resizewindow.sh
@@ -0,0 +1,26 @@
+
+export DISPLAY=:20
+
+loadpid=$(xdotool search --pid $(pidof java))
+
+until [[ ! -z "$loadpid" ]]
+do
+        loadpid=$(xdotool search --pid $(pidof java))
+        curpid=$(xdotool search --pid $(pidof java))
+done
+
+echo "loadpid: $loadpid"
+
+until [[ $curpid != *$loadpid* ]]
+do
+        echo "window not loaded"
+        sleep .5
+        curpid=$(xdotool search --pid $(pidof java))
+done
+
+for n in $(xdotool search --pid $(pidof java))
+do
+        sleep 2
+        echo "window loaded. resizing"
+        xdotool windowsize $n 100% 100%
+done
